2e4494143d06ec49cddf0eab23411f752eb76aac
BrotherPlayerBR <pedrolucas443@gmail.com>
2022-02-11 02:41:54 -0300

    fix html

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 $(document).ready(function(){
   $("#getMap").hide();
-
+  $("#local").hide();
+  let cep = ""
   $("#show").click(function(){
-    let cep = $("#cep-num").val();
     cep = cep.replaceAll("-", "");
     cep = cep.replaceAll(".", "");
-    console.log(cep.length)
     if(cep.length == 8){
       $.ajax({ url: `https://cep.awesomeapi.com.br/json/${cep}`})
       .done( (data) => {
         let lat = data.lat;
-        let lng = data.lng
-        console.log(data);
+        let lng = data.lng;
         $("#local").html(data.city + " - " + data.address);
         $("#getMap").attr("src", `https://www.google.com/maps?api=1&q=${lat}%2C${lng}&output=embed`);
         $("#getMap").show();
+        $("#local").show();
       })
       .fail((data) => {
         console.log(data)
@@ -22,8 +21,19 @@ $(document).ready(function(){
    
     }else{
       $("#getMap").hide();
+      $("#local").html("Cep não encontrado!");
+      $("#local").show();
+    }
+  })
+  $("#cep-num").on('input', function(){
+    cep = $("#cep-num").val();
+    console.log(cep.length)
+    if(cep.length == 0){
+      $("#getMap").hide();
+      $("#local").hide();
       $("#local").html("");
     }
   })
+
 
 });
